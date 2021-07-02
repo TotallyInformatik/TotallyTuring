67a27cbec3a90a94f8b1995450c91bbdd3e98d4c
Rui Zhang <totallyturing@gmail.com>
2021-07-02 21:37:38 +0200

    added automatic age calculation

--- js/age-set.js
+++ js/age-set.js
@@ -1,3 +1,15 @@
-function getAge() {
-  
-}
+
+
+$(function() {
+
+
+  const birthdate = new Date(2005, 6, 16);
+  var today = new Date();
+  const diffTime = Math.abs(today - birthdate);
+  const diffYear = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 365)); 
+
+  console.log(diffYear);
+
+  $(".age").text("16");
+
+});
